a067fe01f26a4bbbf6b2c5751123cd7350de9d96
Calixte Henry <131826512+CalHenry@users.noreply.github.com>
2026-02-03 11:29:35 +0100

    tests: data_model
    add test load an empty csv
    test save is WIP
    doc: small updates to the comments of the assert of the tests in
    'test_load'

--- tests/test_data_model.py
+++ tests/test_data_model.py
@@ -9,6 +9,7 @@ from dirty_equals import IsInt, IsList
 from csv_ve.data_model import CSVDataModel
 
 
+# test load
 def test_load_valid_csv_with_headers(temp_csv_with_headers):
     """Test load csv file correctly and with headers and correct shape"""
     csv_path = temp_csv_with_headers
@@ -16,11 +17,29 @@ def test_load_valid_csv_with_headers(temp_csv_with_headers):
     model = CSVDataModel(str(csv_path))
 
     assert model.df is not None, "DataFrame should be loaded"
-    assert model.modified is False, "Newly loaded file should not be modified"
-    assert model.has_header is True, "Should detect headers"
+    assert model.modified is False
+    assert model.has_header is True
     # data schema
-    assert model.row_count() == IsInt(exactly=3), "Should have 3 data rows"
-    assert model.column_count() == IsInt(exactly=3), "Should have 3 columns"
+    assert model.row_count() == IsInt(exactly=3)
+    assert model.column_count() == IsInt(exactly=3)
+    # col names
+    assert model.df.columns == IsList("name", "age", "city"), (
+        "Column names should match"
+    )
+
+
+def test_load_empty_csv(temp_csv_empty):
+    """empty csv only has headers"""
+    csv_path = temp_csv_empty
+
+    model = CSVDataModel(str(csv_path))
+
+    assert model.df is not None, "DataFrame should be loaded"
+    assert model.modified is False
+    assert model.has_header is True
+    # data schema
+    assert model.row_count() == IsInt(exactly=0)
+    assert model.column_count() == IsInt(exactly=3)
     # col names
     assert model.df.columns == IsList("name", "age", "city"), (
         "Column names should match"
@@ -34,6 +53,17 @@ def test_non_existent_file():
     with patch.object(CSVDataModel, "load", return_value=None):
         model = CSVDataModel(str(non_existent_path))
 
-    # manually call `load` and expect filenotfound exception
     with pytest.raises(FileNotFoundError):
         model.load()
+
+
+# test save
+def test_save_raises_if_no_df(tmp_path):
+    csv_path = tmp_path / "test.csv"
+    model = CSVDataModel(str(csv_path))
+
+    # Sanity check: no data loaded
+    assert model.df is None
+
+    with pytest.raises(RuntimeError, match="No data to save"):
+        model.save()
