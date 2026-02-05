2c4072ec111fe81b5c9bcc8bb7fca88726639a77
Calixte Henry <131826512+CalHenry@users.noreply.github.com>
2026-02-05 15:34:17 +0100

    tests: ui
    add tests for G and g key bindings
    remove useless line in other VimKeyBindings tests

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -5,12 +5,13 @@ from csv_ve.ui import CSVEditorApp
 
 # temp_csv_with_headers is a 3x3 csv - First row of the table a labeled row so the row index starts at -1
 class TestBasicKeybinds:
+    "test: q and ctrl+s key behavior"
+
     async def test_quit(self, temp_csv_with_headers):
         "Test that 'q' quit the app"
         app = CSVEditorApp(csv_path=temp_csv_with_headers, theme=None)
 
         async with app.run_test() as pilot:
-            # Press 'q' to quit
             await pilot.press("q")
             assert not app.is_running
 
@@ -18,7 +19,7 @@ class TestBasicKeybinds:
         """
         Test that ctrl+s triggers save and writes to file
         - only test that the modified flag and notify
-        - test on actually saving the file are in data_model.py
+        - test on actually saving the file are in test_data_model.py
         """
         app = CSVEditorApp(csv_path=temp_csv_with_headers, theme=None)
         async with app.run_test() as pilot:
@@ -37,6 +38,8 @@ class TestBasicKeybinds:
 
 
 class TestVimKeybinds:
+    "test: h j k l G and g key behavior"
+
     async def test_table_left(self, temp_csv_with_headers):
         """Pressing 'h' moves the cursor left in the DataTable"""
         app = CSVEditorApp(csv_path=temp_csv_with_headers, theme=None)
@@ -46,7 +49,6 @@ class TestVimKeybinds:
             table.focus()
             await pilot.pause()
 
-            table.move_cursor(column=1)
             initial_column = table.cursor_column
 
             # 'h' should move the cursor
@@ -62,10 +64,8 @@ class TestVimKeybinds:
             app.query_one("#formula_bar").focus()  # unfocus the table
             await pilot.pause()
 
-            table.move_cursor(column=1)
             initial_column = table.cursor_column
 
-            # 'h' should not move the cursor
             await pilot.press("h")
             await pilot.pause()
             assert table.cursor_column == initial_column
@@ -79,10 +79,8 @@ class TestVimKeybinds:
             table.focus()
             await pilot.pause()
 
-            table.move_cursor(row=1)
             initial_row = table.cursor_row
 
-            # 'j' should move the cursor
             await pilot.press("j")
             await pilot.pause()
             assert table.cursor_row == initial_row + 1
@@ -95,10 +93,8 @@ class TestVimKeybinds:
             app.query_one("#formula_bar").focus()  # unfocus the table
             await pilot.pause()
 
-            table.move_cursor(row=1)
             initial_row = table.cursor_row
 
-            # 'j' should not move the cursor
             await pilot.press("j")
             await pilot.pause()
             assert table.cursor_row == initial_row
@@ -112,10 +108,8 @@ class TestVimKeybinds:
             table.focus()
             await pilot.pause()
 
-            table.move_cursor(row=1)
             initial_row = table.cursor_row
 
-            # 'k' should move the cursor
             await pilot.press("k")
             await pilot.pause()
             assert table.cursor_row == initial_row - 1
@@ -128,10 +122,8 @@ class TestVimKeybinds:
             app.query_one("#formula_bar").focus()  # unfocus the table
             await pilot.pause()
 
-            table.move_cursor(row=1)
             initial_row = table.cursor_row
 
-            # 'k' should not move the cursor
             await pilot.press("k")
             await pilot.pause()
             assert table.cursor_row == initial_row
@@ -145,10 +137,8 @@ class TestVimKeybinds:
             table.focus()
             await pilot.pause()
 
-            table.move_cursor(column=1)
             initial_column = table.cursor_column
 
-            # 'l' should move the cursor
             await pilot.press("l")
             await pilot.pause()
             assert table.cursor_column == initial_column + 1
@@ -161,10 +151,65 @@ class TestVimKeybinds:
             app.query_one("#formula_bar").focus()  # unfocus the table
             await pilot.pause()
 
-            table.move_cursor(column=1)
             initial_column = table.cursor_column
-
-            # 'l' should not move the cursor
             await pilot.press("l")
             await pilot.pause()
+
             assert table.cursor_column == initial_column
+
+    async def test_table_bottom(self, temp_csv_with_headers):
+        """Pressing 'G' moves the cursor to the bottom of the DataTable"""
+        app = CSVEditorApp(csv_path=temp_csv_with_headers, theme=None)
+
+        async with app.run_test() as pilot:
+            table = app.query_one(DataTable)
+            table.focus()
+            await pilot.pause()
+            row_count = table.row_count
+
+            await pilot.press("G")
+            await pilot.pause()
+
+            assert table.cursor_row == row_count - 1  # row index starts at 0
+
+    async def test_G_only_works_when_table_focused(self, temp_csv_with_headers):
+        app = CSVEditorApp(csv_path=temp_csv_with_headers, theme=None)
+
+        async with app.run_test() as pilot:
+            table = app.query_one(DataTable)
+            app.query_one("#formula_bar").focus()  # unfocus the table
+            await pilot.pause()
+            initial_row_idx = table.cursor_row
+
+            await pilot.press("G")
+            await pilot.pause()
+
+            assert table.cursor_row == initial_row_idx
+
+    async def test_table_top(self, temp_csv_with_headers):
+        """Pressing 'g' moves the cursor to the top of the DataTable"""
+        app = CSVEditorApp(csv_path=temp_csv_with_headers, theme=None)
+
+        async with app.run_test() as pilot:
+            table = app.query_one(DataTable)
+            table.focus()
+            await pilot.pause()
+
+            await pilot.press("g")
+            await pilot.pause()
+
+            assert table.cursor_row == 0  # row index starts at 0
+
+    async def test_g_only_works_when_table_focused(self, temp_csv_with_headers):
+        app = CSVEditorApp(csv_path=temp_csv_with_headers, theme=None)
+
+        async with app.run_test() as pilot:
+            table = app.query_one(DataTable)
+            app.query_one("#formula_bar").focus()  # unfocus the table
+            await pilot.pause()
+            initial_row_idx = table.cursor_row
+
+            await pilot.press("g")
+            await pilot.pause()
+
+            assert table.cursor_row == initial_row_idx
