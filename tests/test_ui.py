81707c14ed2956f799daaf90e5ad26570a42a1e4
Calixte Henry <131826512+CalHenry@users.noreply.github.com>
2026-02-09 15:52:45 +0100

    tests: changed TestSave to TestNotifications. save is already tested in 'test_ctrl_s_saves_file' New class to test all the notifs of the app, like if they go away coorectly after a while

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,3 +1,7 @@
+import asyncio
+
+from dirty_equals import Contains, HasLen
+from textual.notifications import Notification
 from textual.widgets import DataTable
 
 from csv_ve.ui import CSVEditorApp
@@ -24,15 +28,13 @@ class TestBasicKeybinds:
         app = CSVEditorApp(csv_path=temp_csv_with_headers, theme=None)
         async with app.run_test() as pilot:
             app.data_model.modified = True
-            # Simulate Ctrl+S keypress
+
             await pilot.press("ctrl+s")
             await pilot.pause()
 
             # check the file was actually written
             assert temp_csv_with_headers.exists()
-            # check modified flag is cleared
             assert app.data_model.modified is False
-            # check the notify
             notifications = app._notifications
             assert any("Saved" in n.message for n in notifications)
 
@@ -48,12 +50,12 @@ class TestVimKeybinds:
             table = app.query_one(DataTable)
             table.focus()
             await pilot.pause()
-
+            table.move_cursor(column=1)
             initial_column = table.cursor_column
 
-            # 'h' should move the cursor
             await pilot.press("h")
             await pilot.pause()
+
             assert table.cursor_column == initial_column - 1
 
     async def test_h_only_works_when_table_focused(self, temp_csv_with_headers):
@@ -63,11 +65,11 @@ class TestVimKeybinds:
             table = app.query_one(DataTable)
             app.query_one("#formula_bar").focus()  # unfocus the table
             await pilot.pause()
-
             initial_column = table.cursor_column
 
             await pilot.press("h")
             await pilot.pause()
+
             assert table.cursor_column == initial_column
 
     async def test_table_down(self, temp_csv_with_headers):
@@ -78,11 +80,11 @@ class TestVimKeybinds:
             table = app.query_one(DataTable)
             table.focus()
             await pilot.pause()
-
             initial_row = table.cursor_row
 
             await pilot.press("j")
             await pilot.pause()
+
             assert table.cursor_row == initial_row + 1
 
     async def test_j_only_works_when_table_focused(self, temp_csv_with_headers):
@@ -92,11 +94,11 @@ class TestVimKeybinds:
             table = app.query_one(DataTable)
             app.query_one("#formula_bar").focus()  # unfocus the table
             await pilot.pause()
-
             initial_row = table.cursor_row
 
             await pilot.press("j")
             await pilot.pause()
+
             assert table.cursor_row == initial_row
 
     async def test_table_up(self, temp_csv_with_headers):
@@ -107,11 +109,12 @@ class TestVimKeybinds:
             table = app.query_one(DataTable)
             table.focus()
             await pilot.pause()
-
+            table.move_cursor(row=1)
             initial_row = table.cursor_row
 
             await pilot.press("k")
             await pilot.pause()
+
             assert table.cursor_row == initial_row - 1
 
     async def test_k_only_works_when_table_focused(self, temp_csv_with_headers):
@@ -121,11 +124,11 @@ class TestVimKeybinds:
             table = app.query_one(DataTable)
             app.query_one("#formula_bar").focus()  # unfocus the table
             await pilot.pause()
-
             initial_row = table.cursor_row
 
             await pilot.press("k")
             await pilot.pause()
+
             assert table.cursor_row == initial_row
 
     async def test_table_right(self, temp_csv_with_headers):
@@ -136,11 +139,11 @@ class TestVimKeybinds:
             table = app.query_one(DataTable)
             table.focus()
             await pilot.pause()
-
             initial_column = table.cursor_column
 
             await pilot.press("l")
             await pilot.pause()
+
             assert table.cursor_column == initial_column + 1
 
     async def test_l_only_works_when_table_focused(self, temp_csv_with_headers):
@@ -150,8 +153,8 @@ class TestVimKeybinds:
             table = app.query_one(DataTable)
             app.query_one("#formula_bar").focus()  # unfocus the table
             await pilot.pause()
-
             initial_column = table.cursor_column
+
             await pilot.press("l")
             await pilot.pause()
 
@@ -213,3 +216,43 @@ class TestVimKeybinds:
             await pilot.pause()
 
             assert table.cursor_row == initial_row_idx
+
+
+class TestLoadData:
+    "test: load_data()"
+
+    async def test_load_data_no_data(slef, temp_csv_with_headers):
+        app = CSVEditorApp(temp_csv_with_headers, theme=None)
+        async with app.run_test() as pilot:
+            app.data_model.df = None
+            app.load_data()
+            await pilot.pause()
+
+            assert app.sub_title == "No data loaded"
+
+    async def test_load_data_row(self, temp_csv_with_headers):
+        app = CSVEditorApp(temp_csv_with_headers, theme=None)
+
+        async with app.run_test() as pilot:
+            app.load_data()
+            await pilot.pause()
+            table = app.query_one(DataTable)
+
+            assert table.row_count == 3
+            assert table.columns == HasLen(3)
+            assert app.sub_title == Contains("test_data.csv | 3 rows × 3 cols")
+
+
+class TestNotifications:
+    "test: notifications of the app with 'app.notify()"
+
+    async def test_notifications_expires(self, temp_csv_with_headers) -> None:
+        """Save notifications should expire from an app."""
+        app = CSVEditorApp(temp_csv_with_headers, theme=None)
+        async with app.run_test() as pilot:
+            await pilot.press("ctrl+s")
+
+            assert len(pilot.app._notifications) == 1
+            # notifications last for ~4 sec
+            await asyncio.sleep(5)
+            assert len(pilot.app._notifications) == 0
